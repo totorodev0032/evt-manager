a5f9251e0f7156ede1906ba67133a85f9b2cada0
nagendra0032 <nagendrakumar0032@gmail.com>
2021-06-25 15:07:29 +0530

    action creator added sandbox

--- src/components/sandbox/Sandbox.js
+++ src/components/sandbox/Sandbox.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { Button } from 'semantic-ui-react';
-import { DECREMENT_COUNTER, INCREMENT_COUNTER } from './testReducer';
+import { decrement, increment } from './testReducer';
 
 const Sandbox = () => {
   const dispatch = useDispatch();
@@ -11,12 +11,12 @@ const Sandbox = () => {
       <h1>Testing</h1>
       <h3>the data is: {data}</h3>
       <Button
-        onClick={() => dispatch({ type: INCREMENT_COUNTER })}
+        onClick={() => dispatch(increment(20))}
         content="Increment"
         color="green"
       />
       <Button
-        onClick={() => dispatch({ type: DECREMENT_COUNTER })}
+        onClick={() => dispatch(decrement(10))}
         content="Decrement"
         color="red"
       />
